cdce1fe33a499bccde4b09a35387eaabd5bd0b25
anandneredimilli <paulanand926@gmail.com>
2023-07-07 19:39:54 +0530

    added data method

--- flight_search.py
+++ flight_search.py
@@ -1,4 +1,5 @@
 import requests
+import flight_data
 class FlightSearch:
     def __init__(self,FLY_FROM,FLY_TO,FROM_DATE,TO_DATE) -> None:
         self.journey_dict ={
@@ -10,11 +11,20 @@ class FlightSearch:
     def search(self):
         api_key = {"apikey":"xm44mZXeSlwUrwhpzedbqraXXRCzQ22z"}
         data = requests.get(url="https://api.tequila.kiwi.com/v2/search", headers=api_key, params=self.journey_dict).json()
-        print(data['data'][0]['cityFrom'])
-        print(data['data'][0]['cityTo'])
-        print(data['data'][0]['price'])
-        print(data['data'][0]['route'][0]['cityFrom'])
-        print(data['data'][0]['route'][0]['cityTo'])
-        print((data['data'][0]['route'][0]['local_departure']).split("T"))
-        print((data['data'][0]['route'][0]['local_arrival']).split("T"))        
+        # print(data['data'][0]['cityFrom'])
+        # print(data['data'][0]['cityTo'])
+        # print(data['data'][0]['price'])
+        # print(data['data'][0]['route'][0]['cityFrom'])
+        # print(data['data'][0]['route'][0]['cityTo'])
+        # print((data['data'][0]['route'][0]['local_departure']).split("T"))
+        # print((data['data'][0]['route'][0]['local_arrival']).split("T"))
+
+        flightdata =   flight_data.FlightData(
+                                        price=data['price'],
+                                        flying_from=data['cityFrom'],
+                                        flying_to=data["cityTo"],
+                                        date_of_journey=data['route'][0]['local_departure'].split("T")[0],
+                                        time_of_journey=data['route'][0]['local_departure'].split("T")[1])
+    
+        return flightdata        
     
